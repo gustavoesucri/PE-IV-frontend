7c927d62a434d470e389f20051801159c53cbfd7
gustavoesucri <gustavo.esucri@gmail.com>
2025-09-19 15:17:11 -0300

    esqueleto HTML page FollowUp

--- src/pages/FollowUp/FollowUp.jsx
+++ src/pages/FollowUp/FollowUp.jsx
@@ -1,5 +1,22 @@
 const FollowUp = () => {
-    return <h1>FollowUp</h1>
+    return <>    
+    <h1>FollowUp</h1>
+
+    <p>Nome: (importar)</p>
+
+    <p>Data de Admissão: (talvez seja importado, talvez não)</p>
+
+    <p>Empresa: (importar)</p>
+
+    <p>Responsável RH: (importar)</p>
+
+    <p>Data da visita:</p>
+
+    <p>Contato com: (por enquanto, campo aberto de string)</p>
+
+    <p>Parecer Geral: (textarea abaixo)</p>
+    <textarea></textarea>
+    </>
 }
 
 export default FollowUp;
